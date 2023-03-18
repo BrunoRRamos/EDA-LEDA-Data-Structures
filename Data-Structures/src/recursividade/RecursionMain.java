ceeee5af24586ed7290e02c2dd83c1177789f0d6
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-03-18 16:49:55 -0300

    feat: Resurssion training

---
+++ Data-Structures/src/recursividade/RecursionMain.java
@@ -0,0 +1,12 @@
+package recursividade;
+
+public class RecursionMain {
+    public static void main(String[] args) {
+        RecurssionClass testRecursiveMethod = new RecurssionClass();
+
+        //System.out.println(testRecursiveMethod.countTo(10));
+        testRecursiveMethod.populateArray();
+        testRecursiveMethod.returnCouples();
+
+    }
+}
---
+++ Data-Structures/src/recursividade/RecurssionClass.java
@@ -0,0 +1,59 @@
+package recursividade;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static java.lang.System.exit;
+
+public class RecurssionClass {
+    private int count;
+    private int[] arr;
+
+    public RecurssionClass() {
+        this.count = 0;
+        this.arr = new int[10];
+    }
+
+    public int countTo(int ceilValue) {
+        if (this.count < ceilValue) {
+            System.out.println(count);
+            count ++;
+            countTo(ceilValue);
+        }
+        return count;
+    }
+
+    public void populateArray() {
+        if (this.count < getArrlegth()) {
+           arr[count] = count;
+           count ++;
+           populateArray();
+        }
+        resetCount();
+    }
+
+    public void returnCouples() {
+        if (count < arr.length && count != 0) {
+            if (arr[count] % 2 == 0) {
+                System.out.println(arr[count]);
+                count++;
+                returnCouples();
+            }
+        } else {
+            count++;
+            returnCouples();
+        }
+
+        resetCount();
+        exit(0);
+    }
+
+    public int getArrlegth() {
+        return this.arr.length;
+    }
+
+    public void resetCount() {
+        this.count = 0;
+    }
+}
