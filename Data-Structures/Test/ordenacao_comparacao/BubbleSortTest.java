d6e59ae2cd101c1ae077b4ca1b163aa47ece4e74
Bruno Rodrigues Ramos <brbosshd2@gmail.com>
2023-04-04 18:08:53 -0300

    feat: Selection sort & Tests

--- Data-Structures/Test/ordenacao_comparacao/BubbleSortTest.java
+++ Data-Structures/Test/ordenacao_comparacao/BubbleSortTest.java
@@ -2,10 +2,8 @@ package ordenacao_comparacao;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSortTest {
@@ -13,12 +11,14 @@ class BubbleSortTest {
     BubbleSort bubbleSort = new BubbleSort();
     SelectionSort selectionSort = new SelectionSort();
     int[] arr = new int[40];
+
     @BeforeEach
     void criaArrTest() {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = ramdom.nextInt(101);
         }
     }
+
     @Test
     void bubbleSort() {
         int[] mySorted = bubbleSort.bubbleSort(arr);
--- Data-Structures/Test/ordenacao_comparacao/SelectionSortTest.java
+++ Data-Structures/Test/ordenacao_comparacao/SelectionSortTest.java
@@ -2,22 +2,22 @@ package ordenacao_comparacao;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SelectionSortTest {
     Random ramdom = new Random();
     SelectionSort selectionSort = new SelectionSort();
     int[] arr = new int[40];
+
     @BeforeEach
     void criaArrTest() {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = ramdom.nextInt(101);
         }
     }
+
     @Test
     void selectionSort() {
         int[] mySorted = selectionSort.selectionSort(arr);
